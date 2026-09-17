4686b8afd825b82cdd0882d825faefd2e1bd809a
Yunish Gurung <gurungyunish825@gmail.com>
2026-09-17 15:00:53 +0545

    Replace Suhag references in mixins.py

--- mixins.py
+++ mixins.py
@@ -23,4 +23,4 @@ class User(LoggerMixin, SerializerMixin):
 
 if __name__ == "__main__":
     print("--- Initializing User Class (Triggering LoggerMixin) ---")
-    user = User(username="adsuhag", email="suhag@example.com", role="Student")
+    user = User(username="adyunish", email="yunish@example.com", role="Student")
