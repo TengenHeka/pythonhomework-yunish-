bb87ff30bf72a1481aee1a30bea212987f8ec8b4
Yunish Gurung <gurungyunish825@gmail.com>
2026-09-17 15:25:01 +0545

    Add files via upload

---
+++ yunish-assignment/method1.py
@@ -0,0 +1,12 @@
+import method
+
+result = method.add(5, 3)
+print(result)
+from method import subtract
+result = subtract(10, 4)
+print(result)   
+
+import method as mo
+result = mo.add(7, 2)
+print(result)
+
